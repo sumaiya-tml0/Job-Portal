a155b2ed82704616b5dad028fd6d406261473839
sumaiya-tml0 <Sumaiya@technometrics.net>
2025-12-04 16:02:55 +0600

    created login, logout routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { register } from "../controller/authController.js";
+import { register, login } from "../controller/authController.js";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", (req, res) => {
+  res.status(200).json({ message: "Logout successful" });
+});
 
 export default router;
